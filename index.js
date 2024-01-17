bb0abe9114e1a851e308444c473a003a169e3396
Davit-Kunchulia <148901201+Davit-Kunchulia@users.noreply.github.com>
2024-01-17 16:46:16 +0400

    1

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// 1
+for (let i = 5; i <= 100; i = i + 1) {
+    console.log(i);
+}
+
+
+// 2
+// for (let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12]; array1 > 0; array1 < 10) {
+//     console.log(array1);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
