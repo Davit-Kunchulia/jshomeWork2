c06d638798826e7bcb729d91615c9d8eb7b7f19f
Davit-Kunchulia <148901201+Davit-Kunchulia@users.noreply.github.com>
2024-01-18 21:58:28 +0400

    half

--- index.js
+++ index.js
@@ -5,10 +5,86 @@ for (let i = 5; i <= 100; i = i + 1) {
 }
 
 
-// 2
-// for (let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12]; array1 > 0; array1 < 10) {
-//     console.log(array1);
-// }
+// 4
+let array3 = [1, 2, 3, 4, 5];
+
+let sum = 0
+
+for (let mimateba = 0; mimateba < array3.length; mimateba++) {
+    sum = sum + array3[mimateba]
+}
+console.log(sum);
+
+
+// 5
+let array4 = [1, 2, 3, 7, 6, 9];
+
+let sum2 = 0;
+
+for (let average = 0; average < array4.length; average++) {
+    sum2 = sum2 + array4[average]
+}
+
+console.log(sum2 / array4.length);
+
+
+// 7
+let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus: "active"
+};
+
+console.log(user.studentstatus);
+
+
+// 8
+let user2 = {
+    name: 'giorgi',
+    age: 20,
+    studentstatus: 'active'
+}
+
+if (user2.age < 18 & user2.studentstatus == "active") {
+    console.log("hello");
+}
+if (user2.name == "levani") {
+    console.log("hello levani");
+}
+if (user2.studentstatus == "active" || user2.age < 25) {
+    console.log("hello world");
+}
+else {
+    console.log("error");
+}
+
+
+// 11
+let users = [
+    { username: 'giorgi', status: false },
+    { username: 'levani', status: false },
+    { username: 'anna', status: true }
+]
+
+
+if (users[0].status == true) {
+    console.log(users[0]);
+}
+if (users[1].status == true) {
+    console.log(users[1]);
+}
+if (users[2].status == true) {
+    console.log(users[2]);
+}
+
+
+
+
+
+
+
+
 
 
 
